1ddced7b72b55d3688f568ed4b961cf48864087d
antoine <antoine.cordier@display-interactive.com>
2019-10-11 15:33:30 +0200

    Add momentJSReact

--- front/src/component/Home/home.js
+++ front/src/component/Home/home.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import {Button} from "react-bootstrap";
+import Moment from 'react-moment';
 
 class Home extends Component{
 
@@ -7,6 +8,9 @@ class Home extends Component{
         return (
             <div>
                 <h1>Home</h1>
+                <Moment format="YYYY/MM/DD">
+                    1976-04-19T12:59-0500
+                </Moment>
             </div>
         )
     }
